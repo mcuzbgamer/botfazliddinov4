5e431615234f9697600f6b94c15beb6be31d9fce
mcuzbgamer <111974683+mcuzbgamer@users.noreply.github.com>
2022-11-09 11:53:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ bot.once("spawn", () => {
 })
 bot.on('chat', (username, message) => {
     if (username === 'FamousTrain1733') {
-    if (message.indexOf('(4) !== -1) {
-            var replacement = "4",
+    if (message.indexOf('($) !== -1) {
+            var replacement = "$",
                 toReplace = "",
                 str = message
 
